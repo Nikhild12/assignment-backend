53c6694a19dbc83265a6a6e80ff7aa9fa1795795
soham <sohampatil3000@gmail.com>
2022-05-20 17:45:54 +0530

    bashscript:changes

--- updateDocker.sh
+++ updateDocker.sh
@@ -1,3 +1,3 @@
-sudo docker push public.ecr.aws/m3v8t2s4/nikhil-backend:latest
-sudo docker rm -f nikhil-backend
-sudo docker run -dp 3000:3000 --name ubuntu nikhil-backend 
+docker pull public.ecr.aws/m3v8t2s4/nikhil-backend:latest
+docker container rm -f nikhil-backend
+docker run -dp 3000:3000 --name  nikhil-backend ubuntu
